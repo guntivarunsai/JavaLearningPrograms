e7ca60a96cbf91af309538fb65999a388680b028
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2019-01-28 08:07:10 +0530

    Static polymorphism examples

--- JavaLearningPrograms/src/com/polymorphism/PolyOverriding.java
+++ JavaLearningPrograms/src/com/polymorphism/PolyOverriding.java
@@ -1,26 +1,27 @@
 package com.polymorphism;
 
-class One
-{
-	public void calculate(double x)
-	{
-		System.out.println("Square Value: "+(x*x));
+class One {
+	public void calculate(double x) {
+		System.out.println("Square Value: " + (x * x));
 	}
 }
 
-class Two extends One
-{
+class Two extends One {
 	@Override
-	public void calculate(double x)
-	{
-		System.out.println("Cube Value : "+(x*x*x));
+	public void calculate(double x) {
+		System.out.println("Cube Value : " + (x * x * x));
 	}
 }
+
 public class PolyOverriding {
 
+	static {
+		System.out.println("Vinay");
+	}
+
 	public static void main(String[] args) {
-		
+
 		Two t = new Two();
-		t.calculate(4);	
+		t.calculate(4);
 	}
 }
---
+++ JavaLearningPrograms/src/com/polymorphism/PolyStatic.java
@@ -0,0 +1,24 @@
+package com.polymorphism;
+
+class One1 {
+	public static void calculate(double x) {
+		System.out.println("Square Value : " + (x * x));
+	}
+}
+
+class Two1 extends One1 {
+	public static void calculate(double x) {
+		System.out.println("Cube Value : " + (x * x * x));
+	}
+}
+
+public class PolyStatic {
+
+	public static void main(String[] args) {
+
+		One1 one1 = new Two1();
+		one1.calculate(5);
+		Two1.calculate(5);
+	}
+
+}
