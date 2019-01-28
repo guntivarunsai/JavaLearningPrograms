7af015e6119cbe67a3546d427e65d441888516a6
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2019-01-28 07:46:58 +0530

    Over riding example

---
+++ JavaLearningPrograms/src/com/polymorphism/PolyOverriding.java
@@ -0,0 +1,26 @@
+package com.polymorphism;
+
+class One
+{
+	public void calculate(double x)
+	{
+		System.out.println("Square Value: "+(x*x));
+	}
+}
+
+class Two extends One
+{
+	@Override
+	public void calculate(double x)
+	{
+		System.out.println("Cube Value : "+(x*x*x));
+	}
+}
+public class PolyOverriding {
+
+	public static void main(String[] args) {
+		
+		Two t = new Two();
+		t.calculate(4);	
+	}
+}
