92acc921ad5a06a5a7a2973e8b2783616cb97b5b
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2019-01-28 08:29:34 +0530

    Static polymorphism and final overriding.

---
+++ JavaLearningPrograms/src/com/polymorphism/PolyFinal.java
@@ -0,0 +1,25 @@
+package com.polymorphism;
+
+class A {
+	public final void method1() {
+		System.out.println("Hello");
+	}
+}
+
+class B extends A {
+	public void method2() {
+		B b = new B();
+		
+		b.method1();
+		System.out.println("K");
+	}
+}
+
+public class PolyFinal {
+	public static void main(String[] args) {
+
+		B b = new B();
+		b.method2();
+	}
+
+}
