dc43be453ddf3a7ef0ae4835359e76d8411953bd
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2019-02-15 08:41:30 +0530

    Select and Create Statements

--- JavaLearningPrograms/Databases Quries.sql
+++ JavaLearningPrograms/Databases Quries.sql
@@ -16,7 +16,6 @@ select * from city where countrycode='IND' and population>1000000 order by distr
 
 select * from city where countrycode='IND' and population>1000000 order by 4 desc;
 
-
-
-
-
+create table leaders(lno int not null,leader_name varchar(20), salary float, city_id int,
+					primary key(lno),
+                    foreign key (city_id) references city(id));
