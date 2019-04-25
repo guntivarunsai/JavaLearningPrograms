cca569c9e6f37094b900afb4f0a64fb5e08bc94f
Kalluri Vinay Reddy <vnreddy60@gmail.com>
2019-04-25 08:29:28 +0530

    added test statement.

--- JavaLearningPrograms/src/Main.java
+++ JavaLearningPrograms/src/Main.java
@@ -2,6 +2,13 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Kalluri Vinay Reddy");
+		
+		// @formatter:off
+		 
+		// @formatter:on
+		
+
+		System.out.println("Test");
 	}
 
 }
